c636177d2b6cb887e8e621dfdecf9c1764b8da42
githa12 <159309793+githa12@users.noreply.github.com>
2024-02-13 16:52:23 +0530

    Create manip.cpp

---
+++ manip.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float d=561.68;
+    string n;
+    cout<<"hi "<<endl;
+    cout<<"world"<<ends;
+    cout<<setw(10)<<"geetha"<<endl;
+    cout<<setw(10)<<setfill('*')<<"geetha"<<endl;
+    cout<<setprecision(11)<<d<<endl;
+    cout<<"the world"<<flush<<endl;
+    cout.flush();
+    cout<<"enter name : ";
+    cin>>ws;
+    cin>>n;
+    cout<<"the name is "<<n<<endl;
+}
+
+
